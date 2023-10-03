077c5e25befa36481ecf174c5bdec7bd131519b6
Oscar Leal <oscarleal234@gmail.com>
2023-10-02 21:45:22 -0500

    starter code

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <img src="./AirBnbLogo.png" />
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
